4f1d3cb99789a88cb4ec06dac352c345f6ae208f
Yetkin Timocin <ytimocin@gmail.com>
2020-01-10 17:34:00 -0800

    Playing around with boto3 and kinesis

--- aws-boto3-experiments/src/kinesis/kinesis.py
+++ aws-boto3-experiments/src/kinesis/kinesis.py
@@ -4,6 +4,27 @@ session = boto3.Session(profile_name='ytimocin')
 
 client = session.client('kinesis')
 
-response = client.list_streams()
+list_of_streams = client.list_streams()
 
-print(response)
+print(list_of_streams)
+
+# create_test_stream = client.create_stream(StreamName='test_stream', ShardCount=1)
+#
+# print(create_test_stream)
+
+put_record_to_test_stream = client.put_record(
+    StreamName='test_stream',
+    Data=b'bytes',
+    PartitionKey='test',
+    ExplicitHashKey='123456',
+    SequenceNumberForOrdering='1'
+)
+
+print(put_record_to_test_stream)
+
+get_records_from_test_stream = client.get_records(
+    ShardIterator='test',
+    Limit=123
+)
+
+print(get_records_from_test_stream)
